e75109a4fd0bafeb7ae64798cf4ec13f90cd5c0e
Kb-tay <taykaiboon12@gmail.com>
2023-01-07 16:48:02 +0800

    Update to ss & comms form

--- src/Forms/SocialForm.js
+++ src/Forms/SocialForm.js
@@ -13,42 +13,79 @@ import {
   Button
 } from '@chakra-ui/react'
 
-const SocialForm = () => {
-  const [role, setRole] = useState('');
+const SocialForm = ({newItem, setNewItem, addItem}) => {
+  const [role, setRole] = useState('member');
+  const [name, setName] = useState('');
+  const [point, setPoint] = useState(0);
 
   useEffect(() => {
-    console.log(role)
-  }, [role])
-  
+    if(name !== '') {
+      const val = assignPoints(name)
+      setPoint(val)
+    } 
+  }, [role, name])
+
+  useEffect(() => {
+    let num = point.toFixed(2)
+    setNewItem({item: name + ' ' + role, points: num})
+  }, [name, point])
+
+  console.log(point)
+
   const data = [
-    {item: 'Eusoff Expeditions', points: role == 'exco' ? 17 : role == 'head' ? 19 : 12},
-    {item: 'Green Committee', points: role == 'exco' ? 7 : role == 'head' ? 11 : 5},
-    // {item: 'Elderly Services', points: 7},
-    // {item: 'MINDS', points: 6},
-    // {item: 'Salvation Army', points: 6}
+    {item: 'Eusoff Expeditions', points: role == 'exco' ? (.7 * 12) + 5: role == 'head' ? (.7 * 12) + 7: (.7 * 12)},
+    {item: 'Green Committee', points: role == 'exco' ? (.7 * 5) + 2: role == 'head' ? (.7 * 5) + 6: (.7 * 5)},
+    {item: 'Elderly Services', points: role == 'exco' ? .7 * 7 + 2: role == 'head' ? .7 * 7 + 4: .7 * 7 },
+    {item: 'MINDS', points: role == 'exco' ? .7 * 6 + 6: role == 'head' ? .7 * 6 + 6: .7 * 6},
+    {item: 'Sports Management Comm', points: role == 'exco' ? .7 * 11 + 6: role == 'head' ? .7 * 11 + 10: .7 * 11},
+    {item: 'IHG Convening', points: role == 'exco' ? .7 * 11 + 6: role == 'head' ? .7 * 11 + 10: .7 * 11},
+    {item: 'Block Comm', points: role == 'exco' ? .7 * 7: role == 'head' ? .7 * 7: .7 * 7},
+    {item: 'Special Projects', points: role == 'exco' ? .7 * 11 + 4: role == 'head' ? .7 * 11 + 5: .7 * 11},
+    {item: 'Hall Relation Board', points: role == 'exco' ? .7 * 11 + 4: role == 'head' ? .7 * 11 + 6: .7 * 11},
+    {item: 'Hall Promotion Board', points: role == 'exco' ? .7 * 7 + 4: role == 'head' ? .7 * 7 + 6: .7 * 7},
+    {item: 'Auditor Comm', points: role == 'exco' ? .7 * 6 + 4: role == 'head' ? .7 * 6 + 4: .7 * 6},
+    {item: 'Elections Comm', points: role == 'exco' ? .7 * 5 + 4: role == 'head' ? .7 * 5 + 4: .7 * 5},
+    {item: 'Finance Comm', points: role == 'exco' ? .7 * 7 + 4: role == 'head' ? .7 * 7 + 4: .7 * 7},
+    {item: 'Eusoff Hackers', points: role == 'exco' ? .7 * 7: role == 'head' ? .7 * 7 + 5: .7 * 7},
+    {item: 'Eusoff External Relations', points: role == 'exco' ? .7 * 8 + 5: role == 'head' ? .7 * 8 + 5: .7 * 8}
   ]
 
   const roles = [
     {id: 1,
-     name: 'member',
-     value: 'member'},
+     name: 'member'},
     {id: 2, 
      name: 'exco'},
     {id: 3,
      name: 'head'},
   ]
 
+  const assignPoints = (value) => {
+    for (let i = 0; i < data.length; i++ ) {
+      if(data[i].item === value) {
+        return data[i].points
+      } 
+    }
+  }
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
   const handleRole = (e) => {
-    console.log(e.target.value)
     setRole(e.target.value)
   }
 
+  const handlenewSubmit = (e) => {
+    e.preventDefault()
+    addItem(newItem)
+  }
+
   return (
-    <form>
+    <form onSubmit={handlenewSubmit}>
     <FormControl>
-      <FormLabel>Social Services hello!</FormLabel>
+      <FormLabel>Social Services & Other Comms</FormLabel>
       <HStack>
-        <Select placeholder='Select Community'>
+        <Select placeholder='Select Community' onChange={handleChange}>
         {
           data.map(item => 
             <option key={item.item} value={item.item}>{item.item}</option>              
@@ -56,13 +93,12 @@ const SocialForm = () => {
         }
         </Select>
         <FormLabel>Role:</FormLabel>
-        <RadioGroup onChange={handleRole}>
-          <HStack>
-            {roles.map((role) => (
-              <Radio key={role.id} value={role.name}>{role.name}</Radio>
-            ))}
-          </HStack>
-        </RadioGroup>
+          <Select onChange={handleRole}>
+            {roles.map(role => 
+              <option key={role.id} value={role.name}>
+                {role.name}
+              </option>)}
+          </Select>
         <Button
             colorScheme='teal'
             type='submit'
--- src/Score.js
+++ src/Score.js
@@ -1,9 +1,9 @@
 const Score = ({items}) => {
-  const total = items.reduce((acc, curr) => acc + parseInt(curr.points), 0);
-  
+  const total = items.reduce((acc, curr) => acc + parseFloat(curr.points), 0);
+  const round = Math.round(total)
   return (
     <ul>
-      <p className="solid">Your Points: {total}</p>
+      <p className="solid">Your Points: {round}</p>
     </ul>
     
   )
