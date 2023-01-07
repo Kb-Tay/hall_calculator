b469f692ff1199cdc0a0a5e67378709369c34cd0
Ashley <ashleyyongwork@gmail.com>
2023-01-07 16:27:21 +0800

    commit2

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p> Disclaimer: This calculator is meant to give a rough estimate of your points. Actual points may differ due to factors such as attendance and playing duration during IHG. </p>
+    </footer>
+  )
+}
+
+export default Footer
