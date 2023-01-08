463af88bcb843ccb57a42076aea28fc141ac760a
Dick Ong <dickongwd@gmail.com>
2023-01-08 21:56:16 +0800

    Refactor styles to use chakra ui

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,37 +1,38 @@
-import { useState } from 'react';
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Button
-} from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Box, Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
 
-const SearchBar = ({cat, setCat}) => {
-  const [open, setOpen] = useState(false);
+const SearchBar = ({ cat, setCat }) => {
+  const [open, setOpen] = useState(false)
 
   const handleOpen = () => {
-    setOpen(!open);
-  };
-
+    setOpen(!open)
+  }
 
   return (
-    <Menu>
-      {({ isOpen }) => (
-        <>
-          <MenuButton isActive={isOpen} as={Button} rightIcon={<ChevronDownIcon />}>
-            {isOpen ? 'Close' : 'Select CCA'}
-          </MenuButton>
-          <MenuList>
-            <MenuItem value={'dp'} onClick={e => setCat(e.target.value)}>Dance Production</MenuItem>
-            <MenuItem value={'sport'} onClick={e => setCat(e.target.value)}>Sports</MenuItem>
-            <MenuItem value={'ss'} onClick={e => setCat(e.target.value)}>Social Service / Other Comms</MenuItem>
-          </MenuList>
-        </>
-      )}
-    </Menu>
-  );
-};
+    <Box mt={10}>
+      <Menu>
+        {({ isOpen }) => (
+          <>
+            <MenuButton isActive={isOpen} as={Button} rightIcon={<ChevronDownIcon />}>
+              {isOpen ? 'Close' : 'Select CCA'}
+            </MenuButton>
+            <MenuList>
+              <MenuItem value={'dp'} onClick={(e) => setCat(e.target.value)}>
+                Dance Production
+              </MenuItem>
+              <MenuItem value={'sport'} onClick={(e) => setCat(e.target.value)}>
+                Sports
+              </MenuItem>
+              <MenuItem value={'ss'} onClick={(e) => setCat(e.target.value)}>
+                Social Service / Other Comms
+              </MenuItem>
+            </MenuList>
+          </>
+        )}
+      </Menu>
+    </Box>
+  )
+}
 
-export default SearchBar;
+export default SearchBar
