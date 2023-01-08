8c67fdbea57b4e0cb05cda7ca7ffaf6a4738863e
Dick Ong <dickongwd@gmail.com>
2023-01-08 22:03:25 +0800

    Added back points

--- src/ItemList.js
+++ src/ItemList.js
@@ -22,7 +22,10 @@ const ItemList = ({ items, handleCheck, handleDelete }) => {
         <Stack divider={<StackDivider />}>
           {items.map((item) => (
             <Flex w="100%" p={2} align="center" justify="space-between" key={item.id}>
-              <Text>{item.name}</Text>
+              <Stack direction="row" spacing={5}>
+                <Text>{item.name}</Text>
+                <Text>{item.points}</Text>
+              </Stack>
               <Spacer />
               <IconButton icon={<FaTrashAlt />} onClick={() => handleDelete(item.id)} />
             </Flex>
